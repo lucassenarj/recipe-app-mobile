5ca682744dfbe2f44b744fc40596abdd07a10b9e
Lucas Sena <lucassena.rj@gmail.com>
2022-01-11 00:17:19 +0000

    feat(types): add types

---
+++ src/types/category.ts
@@ -0,0 +1,8 @@
+type ICategory = {
+  idCategory: number;
+  strCategory: string;
+  strCategoryThumb: string;
+  strCategoryDescription: string;
+};
+
+export default ICategory;
---
+++ src/types/recipe.ts
@@ -0,0 +1,12 @@
+type IRecipe = {
+  idMeal: string;
+  strMeal: string;
+  strCategory: string;
+  strArea: string;
+  strInstructions: string;
+  strMealThumb: string;
+  strTags: string;
+  strYoutube: string,
+};
+
+export default IRecipe;
