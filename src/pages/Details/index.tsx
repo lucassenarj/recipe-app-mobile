548e279b63b113cd6acfe4c063ab8cd8bfc1a012
Lucas Sena <lucassena.rj@gmail.com>
2022-01-12 23:05:26 +0000

    feat(pages): add FavoritesProvider to recipe details page

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,22 +1,31 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import { Text } from "react-native";
 import Header from "../../components/Header";
 import Recipe from "../../components/Recipe";
+import { FavoritesProvider } from "../../context/FavoritesContext";
+import createRecipeDetailsResource from "../../resources/recipeDetailsResource";
 
 import { Wrapper, Container, Main } from "./styles";
 
 function Details({ route, navigation }) {
+  const { params: { id } } = route;
+  const [resource, setResource] = useState(createRecipeDetailsResource(id));
+
   return (
-    <Wrapper>
-      <Container>
-        <Header navigation={navigation} />
-        <Main>
+    <FavoritesProvider>
+      <Wrapper>
+        <Container>
           <Suspense fallback={<Text>Loading...</Text>}>
-            <Recipe id={route.params.id} />
+            <Header resource={resource} navigation={navigation} />
           </Suspense>
-        </Main>
-      </Container>
-    </Wrapper>
+          <Main>
+            <Suspense fallback={<Text>Loading...</Text>}>
+              <Recipe resource={resource} />
+            </Suspense>
+          </Main>
+        </Container>
+      </Wrapper>
+    </FavoritesProvider>
   );
 };
 
