55e104d164c9f54caf6c4511048af3bfc23022de
Lucas Sena <lucassena.rj@gmail.com>
2022-01-13 20:11:09 +0000

    fix(pages): fix homepage

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,28 +1,38 @@
-import React, { Suspense, useState } from "react";
-import { Text } from "react-native";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Recipe from "../../components/Recipe";
 import { FavoritesProvider } from "../../context/FavoritesContext";
-import createRecipeDetailsResource from "../../resources/recipeDetailsResource";
+import getRecipeByIdRequest from "../../requests/getRecipeByIdRequest";
+import IRecipe from "../../types/recipe";
 
 import { Wrapper, Container, Main } from "./styles";
 
 function Details({ route, navigation }) {
   const { params: { id } } = route;
-  const [resource, setResource] = useState(createRecipeDetailsResource(id));
+  const [recipe, setRecipe] = useState<IRecipe | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await getRecipeByIdRequest(id);
+      const { meals } = results;
+      setRecipe(meals[0]);
+    })();
+  }, []);
 
   return (
     <FavoritesProvider>
       <Wrapper>
         <Container>
-          <Suspense fallback={<Text>Loading...</Text>}>
-            <Header resource={resource} navigation={navigation} />
-          </Suspense>
-          <Main>
-            <Suspense fallback={<Text>Loading...</Text>}>
-              <Recipe resource={resource} />
-            </Suspense>
-          </Main>
+          {
+            recipe && (
+              <>
+                <Header recipe={recipe} navigation={navigation} />
+                <Main>
+                  <Recipe recipe={recipe} />
+                </Main>
+              </>
+            )
+          }
         </Container>
       </Wrapper>
     </FavoritesProvider>
