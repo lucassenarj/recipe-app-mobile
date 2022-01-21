980b14ee77a667dfc77ab97453f3f72c688a1ab9
Lucas Sena <lucassena.rj@gmail.com>
2022-01-21 00:13:18 +0000

    feat(types): add rootStackParamList.ts

---
+++ src/types/rootStackParamList.ts
@@ -0,0 +1,12 @@
+type RootStackParamList = {
+  Recipes: undefined;
+  Home: undefined;
+  Details: { id: string };
+  Search: undefined;
+  Favorites: undefined;
+  FavoritesList: undefined;
+  Info: { id: string };
+  About: undefined;
+}
+
+export default RootStackParamList;
